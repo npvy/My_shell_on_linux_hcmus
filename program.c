293e19663bd563054ee0b82e6844fb4f8f669c49
npvy <38617181+npvy@users.noreply.github.com>
2019-10-08 17:04:58 +0700

    Finished!!!

--- program.c
+++ program.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -7,159 +7,14 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 
 #define MAX_LENGTH 256
-#define MAX_ARGS 64
 
 #define MAX_LINE 80 /* The maximum length of a command */
 #define BUFFER_SIZE 50
 #define HISTORY_LIMIT 100
 
-#define REDIRECTION_MODE_NONE 0
-#define REDIRECTION_MODE_IN 1
-#define REDIRECTION_MODE_OUT 2
-
-void removeFileArgs(char **args)
-{
-	int i = 0;
-	while (strcmp(args[i], "<") != 0 && strcmp(args[i], ">") != 0)
-	{
-		i++;
-	}
-	args[i] = NULL;
-	args[i+1] = NULL;
-}
-
-// Xu li chuoi da nhap vao
-void parse(char* commandline, char** args, char* commandArgToken, bool *hasAmpersand, int *redirectionMode, char* redirectedFilepath)
-{
-	const char* delim = " ";
-	commandArgToken = strtok(commandline, delim);
-
-	// Lan luot gan cac tham so da nhap trong commandline vao mang args 
-	int i = 0;
-	int argCount = 0;
-	while (commandArgToken != NULL) 
-	{
-		args[i] = commandArgToken;
-		commandArgToken = strtok(NULL, delim);
-		i++;
-		argCount++;
-	}
-	// Phan tu cuoi cung cua mang args phai la NULL
-	args[i] = NULL;
-
-	if (argCount > 1)
-	{
-		if (strcmp(args[argCount - 1], "&") == 0)
-		{
-			args[argCount - 1] = NULL;
-			*hasAmpersand = true;
-			argCount--;
-		}
-
-		if (argCount > 2)
-		{
-			if (strcmp(args[argCount - 2], "<") == 0)
-			{
-				*redirectionMode = REDIRECTION_MODE_IN;
-				strncpy(redirectedFilepath, args[argCount - 1], sizeof(redirectedFilepath));
-			}
-			if (strcmp(args[argCount - 2], ">") == 0)
-			{
-				*redirectionMode = REDIRECTION_MODE_OUT;
-				strncpy(redirectedFilepath, args[argCount - 1], sizeof(redirectedFilepath));
-			}
-		}
-	}
-}
-
-void execute(char** args, bool hasAmpersand, int redirectionMode, char* redirectedFilepath)
-{
-	pid_t childPID = fork();
-	pid_t pid;
-
-	int status;
-	if (childPID < 0)
-	{
-		// Truong hop khong fork duoc child process
-		printf("[ERROR]: Forking child process failed!\n");
-		exit(1);
-	}
-	else if (childPID == 0)
-	{
-		// Truong hop process dang chay la child process
-
-		int fd_in, fd_out;
-		if (redirectionMode == REDIRECTION_MODE_OUT)
-		{
-			fd_out = open(redirectedFilepath, O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU);
-			if(fd_out == -1) perror("fd: ");
-			dup2(fd_out, 1);
-
-			removeFileArgs(args);
-		}
-
-		if (redirectionMode == REDIRECTION_MODE_IN)
-		{
-			int i = 0;
-			while (strcmp(args[i], "<") != 0) i++;
-
-			fd_in = open(redirectedFilepath, O_RDONLY);
-			if(fd_in == -1) perror("fd: ");
-			dup2(fd_in, 0);
-
-			char commandlineFromInputFile[MAX_LENGTH];
-			fgets(commandlineFromInputFile, MAX_LENGTH, stdin);
-			commandlineFromInputFile[strlen(commandlineFromInputFile)] = '\0';
-
-			const char* delim = " ";
-			char* commandArgToken = NULL;
-			commandArgToken = strtok(commandlineFromInputFile, delim);
-			while (commandArgToken != NULL) 
-			{
-				args[i] = commandArgToken;
-				commandArgToken = strtok(NULL, delim);
-				i++;
-			}
-			// Phan tu cuoi cung cua mang args phai la NULL
-			args[i] = NULL;
-
-
-			removeFileArgs(args);
-		}
-
-		/*int i = 0;
-		while (args[i] != NULL)
-		{
-			printf("%s\n", args[i]);
-			i++;
-		}*/
-
-		// Truyen cac tham so da luu trong args vao execvp
-
-		
-		if (execvp(args[0], args) < 0)
-		{
-			printf("[ERROR]: Exec failed\n");
-			exit(1);
-		}
-
-		close(fd_in);
-		close(fd_out);
-	}
-	else 
-	{
-		// Truong hop process dang chay la parent process
-
-		// Neu trong cac tham so da nhap khong co dau &
-		if (!hasAmpersand)
-		while (wait(&status) != childPID); // Parent process phai cho` child process hoan tat
-	}
-}
-
-//Bo sung
+//History
 void add_history(char* history[], char* commandline, int* count) 
 {
 	int i;
@@ -192,6 +47,133 @@ void add_history(char* history[], char* commandline, int* count)
 		}
 	}
 	*count += 1; // Increment the history count.
+}
+
+// Pipe
+
+void create_pipe(enum cmd_pos pos, int new_pipe[])
+{
+
+	// TODO 2: If there are more than one command in the pipeline,
+	//         create a pipe for all but the last command.
+	switch (pos)
+	{
+	case first:
+	case middle:
+		pipe(new_pipe);
+		break;
+	default:
+		;
+	}
+}
+
+void shift_pipes(int left_pipe[], int right_pipe[])
+{
+
+	left_pipe[0] = right_pipe[0];
+	left_pipe[1] = right_pipe[1];
+}
+void parent_close_pipes(enum cmd_pos pos, int left_pipe[], int right_pipe[]) {
+
+	// TODO 3: The parent must close un-used pipe descriptors. You need
+	// to figure out wich descriptors that must be closes when.
+	switch (pos) {
+	case single:
+		break;
+	case first:
+		//close(right_pipe[1]);
+		break;
+	case middle:
+		close(left_pipe[0]);
+		close(right_pipe[1]);
+		break;
+	case last:
+		close(left_pipe[0]);
+		break;
+	default:
+		;
+	}
+}
+
+void child_redirect_io(enum cmd_pos pos, int left_pipe[], int right_pipe[]) {
+
+	// TODO 4: A child may need to redirect STDIN to read from the left
+	// pipe and STDOUT to write to the right pipe depending on the
+	// position in the pipeline.
+	switch (pos) {
+	case first:
+		dup2(right_pipe[1], 1);
+		break;
+	case middle:
+		dup2(left_pipe[0], 0);
+		dup2(right_pipe[1], 1);
+		break;
+	case last:
+		dup2(left_pipe[0], 0);
+		break;
+	default:
+		;
+	}
+}
+// Xu li chuoi da nhap vao
+void parse(char* commandline, char **args, char* commandArgToken, bool *hasAmpersand)
+{
+	const char* delim = " ";
+	commandArgToken = strtok(commandline, delim);
+
+	// Lan luot gan cac tham so da nhap trong commandline vao mang args 
+	int i = 0;
+	int argCount = 0;
+	while (commandArgToken != NULL)
+	{
+		args[i] = commandArgToken;
+		commandArgToken = strtok(NULL, delim);
+		i++;
+		argCount++;
+	}
+	// Phan tu cuoi cung cua mang args phai la NULL
+	args[i] = NULL;
+
+	if (argCount > 1)
+	if (strcmp(args[argCount - 1], "&") == 0)
+	{
+		args[argCount - 1] = NULL;
+		*hasAmpersand = true;
+	}
+}
+
+void execute(char** args, bool hasAmpersand)
+{
+	pid_t childPID = fork();
+	pid_t pid;
+
+	int status;
+	if (childPID < 0)
+	{
+		// Truong hop khong fork duoc child process
+		printf("[ERROR]: Forking child process failed!\n");
+		exit(1);
+	}
+	else if (childPID == 0)
+	{
+		// Truong hop process dang chay la child process
+		// Truyen cac tham so da luu trong args vao execvp
+		int isPipe;
+		if (execvp(args[0], args) < 0)
+		{
+			printf("[ERROR]: Exec failed\n");
+			exit(1);
+		}
+
+	}
+	else
+	{
+		// Truong hop process dang chay la parent process
+
+		// Neu trong cac tham so da nhap khong co dau &
+		if (!hasAmpersand)
+		while (wait(&status) != childPID); // Parent process phai cho` child process hoan tat
+	}
 }
 
 int main(int argc, char* argv[])
@@ -227,6 +209,7 @@ int main(int argc, char* argv[])
 			should_run = false;
 			exit(0);
 		}
+
 		if (!strcmp(commandline, "history"))
 		{
 			for (i = HISTORY_LIMIT - 1; i >= 0; i--)
@@ -254,24 +237,22 @@ int main(int argc, char* argv[])
 				printf("%s\n", commandline);
 			}
 		}
+
 		// Store user commandline in history, even if its an invalid command. The "history" command is not stored.
 		add_history(history, commandline, &history_count);
 
 		char* commandArgToken = NULL;
 		// Mang luu cac tham so de truyen vao execvp
-		char* args[MAX_ARGS];
+		char* args[64];
 		bool hasAmpersand = false;
 
-		// Flag to handle command differently if it contains a redirection operator "<" or ">"
-		int redirectionMode = REDIRECTION_MODE_NONE; // default is NONE
-		char redirectedFilepath[256] = "";
-
 		// Xu li chuoi lenh da nhap
-		parse(commandline, args, commandArgToken, &hasAmpersand, &redirectionMode, redirectedFilepath);
+		parse(commandline, args, commandArgToken, &hasAmpersand);
 
 		// Thuc thi lenh da nhap
-		execute(args, hasAmpersand, redirectionMode, redirectedFilepath);
+		execute(args, hasAmpersand);
 	}
 
 	return 0;
 }
+
